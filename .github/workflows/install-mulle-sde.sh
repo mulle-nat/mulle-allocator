18de6a82476b3c2c8260b13d6c772dde22ffbe3b
Nat! <nat@mulle-kybernetik.com>
2020-11-23 13:05:52 +0100

    test

--- .github/workflows/install-mulle-sde.sh
+++ .github/workflows/install-mulle-sde.sh
@@ -6,4 +6,4 @@ OTHER_PROJECTS="mulle-c/mulle-c-developer;latest" \
 SDE_PROJECTS="mulle-sde-developer;latest" ./installer-all ~ no
 
 curl -L 'https://github.com/mulle-sde/mulle-test/archive/latest.tar.gz' \
- | tar xfz - && cd 'mulle-test-latest' && ./install ~
+ | tar xfz - && cd 'mulle-test-latest' && ./bin/installer ~
